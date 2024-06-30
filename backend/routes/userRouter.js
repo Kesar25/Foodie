f49ed832bcad77720f71e66958e9cae0873b1e65
Kesar25 <117337016+Kesar25@users.noreply.github.com>
2024-06-30 14:48:43 +0530

    Login User

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,6 +2,7 @@ const User = require("../models/userSchema")
 const { body, validationResult } = require("express-validator")
 const express=require("express");
 const router=express.Router();
+const bcrypt=require('bcrypt')
 
 router.post("/createuser", [body('name', 'Enter a valid name').isLength({ min: 2 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -39,7 +40,30 @@ router.post("/createuser", [body('name', 'Enter a valid name').isLength({ min: 2
 
 router.post("/login",[body('email', 'Please enter a valid email').isEmail(), body('password', 'Password should at least 6 characters long').isLength({min:6})], 
     async(req,res)=>{
-        
+        const {email, password}=req.body;
+    try{
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Please try login with correct credentials"});
+        }
+
+        const comparePass=await bcrypt.compare(password,user.password);
+        if(!comparePass){
+            let success=false
+            return res.status(400).json({success, error:"Please password enter valid credentials"})
+        }
+
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+
+        console.log(user);
+        res.json({success:true, message:"User logged in successfully"})
+    }catch(e){
+        console.log(e)
+    }
     })
     
 
